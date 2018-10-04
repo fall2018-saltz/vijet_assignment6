aa26b6fcae72399f47c764442024fce6ba674bdf
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:42:21 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,8 +1,9 @@
 
 
 no_arg <- function(){                                                             #Creating a function to make sure that all the columns except those mentioned in the vector 'drop'
+  clean_data <- raw_data
   drop <- c("SUMLEV","REGION","DIVISION","STATE")                                 #are deleted from the data frame.
-  dfStates <- dfStates[,!(colnames(dfStates) %in% drop)]                          # %%%%****I am having a doubt regarding this step and while calling the function, the doubt is mentioned below.
+  clean_data <- clean_data[,!(colnames(clean_data) %in% drop)]                          # %%%%****I am having a doubt regarding this step and while calling the function, the doubt is mentioned below.
 }
 
 dfStates <- no_arg() 
